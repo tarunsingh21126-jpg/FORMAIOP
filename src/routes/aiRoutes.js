2824f55948149eeaa50d782dfb86d882e5ab3ab6
Aman Panda <amanpanda@ssipmt.com>
2026-08-22 23:48:49 +0530

    Delete src directory

--- src/routes/aiRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { extractAI } = require('../controllers/aiController');
-
-const router = express.Router();
-router.post('/extract', extractAI);
-
-module.exports = router;
--- src/routes/formRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const { getForm } = require('../controllers/formControllers');
-
-const router = express.Router();
-
-router.get('/:formId', getForm);
-
-module.exports = router;
